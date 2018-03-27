dc88a6b436c10a35fa945df39f2764681687edc0
justinembawomye <jmbawomye@gmail.com>
2018-03-27 15:19:15 +0300

    Added flash messages to layout.html

---
+++ data.py
@@ -0,0 +1,24 @@
+
+import datetime
+def Articles():
+    articles =[
+        {
+        'id':1,
+        'title':'Task 1',
+        'body':'This is my first task',  
+                
+     },
+
+          {
+        'id':200,
+        'title':'Task 2',
+        'body':'This is my second task'
+     },
+
+           {
+        'id':3,
+        'title':'Task 3',
+        'body':'This is my Third task'
+     }
+    ]
+    return  articles
